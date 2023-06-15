6df4d2a484314a2c2bebb1e5b45ea483f2367790
jahid-ahmed-db <jahid.ahmed@db.com>
2023-06-15 10:20:29 +0100

    adding vars

---
+++ auto.tfvars
@@ -0,0 +1,19 @@
+project_id = "jahid-network-test"
+
+region = "us-central1"
+
+network_name = "lean-gke-network"
+
+gke_cluster_name = "lean-gke-cluster"
+
+num_nodes = 1
+
+disk_size = 20
+
+cluster_subnetwork_cidr_range = "10.0.0.0/24"
+
+master_ipv4_cidr_block = "10.1.0.0/28"
+
+cluster_ipv4_cidr_block = "10.2.0.0/18"
+
+services_ipv4_cidr_block = "10.3.0.0/18"
---
+++ variable.tf
@@ -0,0 +1,71 @@
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+  type = string
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+}
+
+variable "zone" {
+  description = "The zone to host the cluster in (required if is a zonal cluster)"
+
+}
+
+variable "network_name" {
+  description = "The name of the network"
+}
+
+variable "gke_cluster_name" {
+  description = "The name of the cluster"
+  default     = "lean-gke-cluster"
+}
+
+variable "num_nodes" {
+  description = "The number of cluster nodes"
+}
+
+variable "disk_size" {
+  description = "The disk size of the cluster nodes"
+}
+
+variable "node_pools" {
+  default = {
+    ingress = {
+      machine_type       = "e2-small"
+      initial_node_count = 1
+      min_node_count     = 1
+      max_node_count     = 1
+      spot               = true
+      auto_repair        = true
+      auto_upgrade       = true
+      disk_size_gb       = 10
+      disk_type          = "pd-standard"
+      tags = [
+        "ingress"
+      ]
+      taints = [
+        {
+          key    = "ingress"
+          value  = true
+          effect = "NO_EXECUTE"
+        }
+      ]
+    }
+    shared = {
+      machine_type       = "e2-small"
+      initial_node_count = 1
+      min_node_count     = 1
+      max_node_count     = 5
+      spot               = true
+      auto_repair        = true
+      auto_upgrade       = true
+      disk_size_gb       = 20
+      disk_type          = "pd-standard"
+      taints             = []
+      tags = [
+        "shared"
+      ]
+    }
+  }
+}
